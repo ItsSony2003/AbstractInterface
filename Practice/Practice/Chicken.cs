3855df7f7acb70c85378a80acf34ec4eb74aca53
ItsSony2003 <sonythaiduong2003@gmail.com>
2024-12-12 17:29:13 +0700

    Create Chicken.cs

---
+++ Practice/Practice/Chicken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Chicken : Animal, IEdible
+    {
+        public override string MakeSound()
+        {
+            return "Chicken: cluck-cluck!";
+        }
+
+        public string HowToEat()
+        {
+            return "could be fried";
+        }
+    }
+}
