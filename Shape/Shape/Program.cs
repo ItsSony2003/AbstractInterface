e72f381cd106ce033882d394dd2e6f97fd6c77e9
ItsSony2003 <sonythaiduong2003@gmail.com>
2025-01-02 20:55:24 +0700

    Update Program.cs

--- Shape/Shape/Program.cs
+++ Shape/Shape/Program.cs
@@ -10,25 +10,27 @@ namespace Shape
     {
         static void Main(string[] args)
         {
-            ComperableCircle[] circles = new ComperableCircle[3];
-            circles[0] = new ComperableCircle(3.6);
-            circles[1] = new ComperableCircle();
-            circles[2] = new ComperableCircle(3.5, "indigo", false);
+            // CircleComparer Test:
+
+            Circle[] circles = new Circle[3];
+            circles[0] = new Circle(3.6);
+            circles[1] = new Circle();
+            circles[2] = new Circle(3.5, "indigo", false);
 
             Console.WriteLine("Pre-sorted:");
-            foreach (ComperableCircle circle in circles)
+            foreach (Circle circle in circles)
             {
                 Console.WriteLine(circle);
             }
 
-            Array.Sort(circles);
+            IComparer<Circle> circleComparator = new ComperableCircle();
+            Array.Sort(circles, circleComparator);
 
             Console.WriteLine("After-sorted:");
-            foreach (ComperableCircle circle in circles)
+            foreach (Circle circle in circles)
             {
                 Console.WriteLine(circle);
             }
-
             Console.ReadKey();
         }
     }
