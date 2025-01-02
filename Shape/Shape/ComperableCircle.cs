d8fba4b4dcfc0d6dbdd9629abada8908edfd2bcc
ItsSony2003 <sonythaiduong2003@gmail.com>
2025-01-02 20:55:23 +0700

    Update ComperableCircle.cs

--- Shape/Shape/ComperableCircle.cs
+++ Shape/Shape/ComperableCircle.cs
@@ -6,18 +6,27 @@ using System.Threading.Tasks;
 
 namespace Shape
 {
-    public class ComperableCircle : Circle, IComparable<ComperableCircle>
+    public class ComperableCircle : Circle, IComparer<Circle>
     {
 
         public ComperableCircle() { }
         public ComperableCircle(double radius) : base(radius) { }
         public ComperableCircle(double radius, string color, bool filled) : base(radius, color, filled) { }
 
+        /*
         public int CompareTo(ComperableCircle o)
         {
             if (getRadius() > o.getRadius()) return 1;
             else if (getRadius() < o.getRadius()) return -1;
             else return 1;
+        }
+        */
+
+        public int Compare(Circle c1, Circle c2)
+        {
+            if (c1.getRadius() > c2.getRadius()) return 1;
+            else if (c1.getRadius() < c2.getRadius()) return -1;
+            else return 0;
         }
     }
 }
